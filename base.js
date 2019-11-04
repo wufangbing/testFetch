745d2255020d1f67f4eb229b481863a9a253f21d
wufangbing <1142768306@qq.com>
2019-11-05 01:05:30 +0800

    Update base.js

--- base.js
+++ base.js
@@ -1,11 +1,1 @@
 const desc = 'this is base';
-const desc1 = 'this is new base';
-const desc2 = 'this is new base';
-const desc3 = 'this is new base';
-const desc4 = 'this is new base';
-const desc5 = 'this is new base';
-const desc6 = 'this is new base';
-const desc7 = 'this is new base';
-const desc9 = 'this is new base';
-
-const desc8 = 'this is new base';
